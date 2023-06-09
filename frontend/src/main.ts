de532888cf299097953ac70bcb83f01d3a8be5cc
Joffrey B <joffreybonifay83@gmail.com>
2023-06-09 08:23:26 +0200

    prepare frontend for clean architecture

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,5 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from "./app/components/app.module";
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
