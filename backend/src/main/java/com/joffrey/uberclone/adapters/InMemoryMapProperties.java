23dd94d51477fe8899d7a31424a768d49d7704af
Joffrey B <joffreybonifay83@gmail.com>
2023-06-11 21:32:24 +0200

    fix bug due to missing property value

--- backend/src/main/java/com/joffrey/uberclone/adapters/InMemoryMapProperties.java
+++ backend/src/main/java/com/joffrey/uberclone/adapters/InMemoryMapProperties.java
@@ -1,9 +1,11 @@
 package com.joffrey.uberclone.adapters;
 
 import com.joffrey.uberclone.domain.ports.MapProperties;
+import org.springframework.beans.factory.annotation.Value;
 
 public class InMemoryMapProperties implements MapProperties {
 
+    @Value("${map.properties.side-size:50}")
     private int sideSize;
 
     @Override
