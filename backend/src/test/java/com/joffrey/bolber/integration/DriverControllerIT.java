6ca1bca98f9e39c0ee209f0960e5b2753a603f9a
Joffrey B <joffreybonifay83@gmail.com>
2023-07-18 11:35:50 +0200

    refactor it

--- backend/src/test/java/com/joffrey/bolber/integration/DriverControllerIT.java
+++ backend/src/test/java/com/joffrey/bolber/integration/DriverControllerIT.java
@@ -3,8 +3,6 @@ package com.joffrey.bolber.integration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -12,10 +10,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ActiveProfiles("integration-test")
 @AutoConfigureMockMvc
-@SpringBootTest
-public class DriverControllerIT {
+public class DriverControllerIT extends AbstractIntegrationTest {
 
     @Autowired
     private MockMvc mvc;
